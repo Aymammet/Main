b21372249ae0623686e7f2b5a2b68834a4b86b82
Aymammet <aymammet2018@gmail.com>
2021-01-10 19:44:44 +0500

    2

--- script/script.js
+++ script/script.js
@@ -5,11 +5,31 @@ document.addEventListener("DOMContentLoaded", async function () {
 
   var body = document.querySelector("body");
   body.style.transition = "all 5s";
-  colors = ["seagreen","aqua", "Chartreuse", "pink", "grey", "blue", "green", "maroon", "orange", 
-            "bisque","CornflowerBlue","Coral" ,"DarkSalmon", "GoldenRod", "LightBlue", "LightGreen", "MediumPurple", 
-            "MediumVioletRed", "PaleGreen", "Sienna","Wheat", "Yellow"];
+  colors = [
+    "seagreen",
+    "aqua",
+    "Chartreuse",
+    "pink",
+    "grey",
+    "blue",
+    "green",
+    "maroon",
+    "orange",
+    "bisque",
+    "CornflowerBlue",
+    "Coral",
+    "DarkSalmon",
+    "GoldenRod",
+    "LightBlue",
+    "LightGreen",
+    "MediumPurple",
+    "MediumVioletRed",
+    "PaleGreen",
+    "Sienna",
+    "Wheat",
+    "Yellow",
+  ];
 
-  
   async function color_change() {
     for (i = 0; i < colors.length; i++) {
       body.style.backgroundColor = colors[i];
@@ -19,5 +39,5 @@ document.addEventListener("DOMContentLoaded", async function () {
   }
 
   color_change();
-  setInterval(color_change, 15000*(colors.length + 1));
+  setInterval(color_change, 15000 * (colors.length + 1));
 });
