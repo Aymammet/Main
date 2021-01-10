2c4f7c080cefdcc12f625363a6104e0f228fc7b9
Aymammet <aymammet2018@gmail.com>
2021-01-10 19:41:49 +0500

    script color changed

--- script/script.js
+++ script/script.js
@@ -4,18 +4,20 @@ document.addEventListener("DOMContentLoaded", async function () {
   }
 
   var body = document.querySelector("body");
-  body.style.transition = "all 1s";
-  colors = ["seagreen","pink", "grey", "blue", "green", "maroon", "orange"];
+  body.style.transition = "all 5s";
+  colors = ["seagreen","aqua", "Chartreuse", "pink", "grey", "blue", "green", "maroon", "orange", 
+            "bisque","CornflowerBlue","Coral" ,"DarkSalmon", "GoldenRod", "LightBlue", "LightGreen", "MediumPurple", 
+            "MediumVioletRed", "PaleGreen", "Sienna","Wheat", "Yellow"];
 
   
   async function color_change() {
     for (i = 0; i < colors.length; i++) {
       body.style.backgroundColor = colors[i];
-      console.log(colors[i]);
-      await sleep(3000);
+      console.log(i, colors[i]);
+      await sleep(15000);
     }
   }
 
   color_change();
-  setInterval(color_change, 3000*(colors.length + 1));
+  setInterval(color_change, 15000*(colors.length + 1));
 });
