6e3138199aedfd2b2851b13fcd002da2a59506ce
Aymammet <aymammet2018@gmail.com>
2022-07-12 00:07:13 +0500

    first step of new design

--- script/script.js
+++ script/script.js
@@ -1,46 +1,46 @@
-document.addEventListener("DOMContentLoaded", async function () {
-  function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
+// document.addEventListener("DOMContentLoaded", async function () {
+//   function sleep(ms) {
+//     return new Promise((resolve) => setTimeout(resolve, ms));
+//   }
 
-  var body = document.querySelector("body");
-  body.style.transition = "all 5s";
-  colors = [
-    "seagreen",
-    "aqua",
-    "Chartreuse",
-    "pink",
-    "grey",
-    "blue",
-    "green",
-    "maroon",
-    "orange",
-    "bisque",
-    "CornflowerBlue",
-    "Coral",
-    "DarkSalmon",
-    "GoldenRod",
-    "LightBlue",
-    "LightGreen",
-    "MediumPurple",
-    "MediumVioletRed",
-    "PaleGreen",
-    "Sienna",
-    "Wheat",
-    "Yellow",
-  ];
+//   var body = document.querySelector("body");
+//   body.style.transition = "all 5s";
+//   colors = [
+//     "seagreen",
+//     "aqua",
+//     "Chartreuse",
+//     "pink",
+//     "grey",
+//     "blue",
+//     "green",
+//     "maroon",
+//     "orange",
+//     "bisque",
+//     "CornflowerBlue",
+//     "Coral",
+//     "DarkSalmon",
+//     "GoldenRod",
+//     "LightBlue",
+//     "LightGreen",
+//     "MediumPurple",
+//     "MediumVioletRed",
+//     "PaleGreen",
+//     "Sienna",
+//     "Wheat",
+//     "Yellow",
+//   ];
 
-  async function color_change() {
-    for (i = 0; i < colors.length; i++) {
-      body.style.backgroundColor = colors[i];
-      console.log(i, colors[i]);
-      await sleep(15000);
-    }
-  }
+//   async function color_change() {
+//     for (i = 0; i < colors.length; i++) {
+//       body.style.backgroundColor = colors[i];
+//       console.log(i, colors[i]);
+//       await sleep(15000);
+//     }
+//   }
 
-  color_change();
-  setInterval(color_change, 15000 * (colors.length + 1));
+//   color_change();
+//   setInterval(color_change, 15000 * (colors.length + 1));
 
  
   
-});
+// });
